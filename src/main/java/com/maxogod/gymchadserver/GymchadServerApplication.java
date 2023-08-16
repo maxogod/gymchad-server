5fa6a09b1232acd3c9e34cd7a7c23ab8976e2075
maxogod <maxi18wars@gmail.com>
2023-08-16 17:57:07 -0300

    init

---
+++ src/main/java/com/maxogod/gymchadserver/GymchadServerApplication.java
@@ -0,0 +1,13 @@
+package com.maxogod.gymchadserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymchadServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymchadServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maxogod/gymchadserver/GymchadServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxogod.gymchadserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymchadServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
